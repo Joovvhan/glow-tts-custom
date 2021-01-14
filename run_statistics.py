b02d26d625c341071d2ee2290fc614da2a7832c9
Joowhan Song <joovvhan.sonq@gmail.com>
2021-01-14 09:30:03 +0000

    File statistics

---
+++ run_statistics.py
@@ -0,0 +1,45 @@
+from glob import glob
+import librosa
+import matplotlib.pyplot as plt
+# import scipy.io.wavfile
+
+def plot_histogram(lengths, keyword):
+    plt.figure(figsize=(6, 6))
+    plt.title(f'{keyword} file length histogram', fontsize=18)
+
+    plt.hist(lengths,
+            bins=20,
+            alpha = 0.5)
+
+    plt.savefig(f'figs/{keyword}_len_hist.png')
+    plt.close()
+    return
+
+CUSTOM = ''
+
+ljspeech_files = sorted(glob('DUMMY/*.wav'))
+kss_files = sorted(glob('DUMMY_K/*/*.wav'))
+custom_files = sorted(glob(f'{CUSTOM}/*.wav'))
+
+print(f'LJSpeech files num | {len(ljspeech_files)}')
+print(f'KSS files num      | {len(kss_files)}')
+print(f'Custom files num   | {len(custom_files)}')
+
+ljspeech_file_lengths = [librosa.core.get_duration(filename=f) for f in ljspeech_files]
+kss_file_lengths = [librosa.core.get_duration(filename=f) for f in kss_files]
+custom_file_lengths = [librosa.core.get_duration(filename=f) for f in custom_files]
+
+plot_histogram(ljspeech_file_lengths, 'ljspeech')
+plot_histogram(kss_file_lengths, 'kss')
+plot_histogram(custom_file_lengths, 'custom')
+
+print(f'LJSpeech length sum  | {sum(ljspeech_file_lengths)}')
+print(f'LJSpeech length mean | {sum(ljspeech_file_lengths)/len(ljspeech_file_lengths)}')
+print(f'KSS      length sum  | {sum(kss_file_lengths)}')
+print(f'KSS      length mean | {sum(kss_file_lengths)/len(kss_file_lengths)}')
+print(f'Custom   length sum  | {sum(custom_file_lengths)}')
+print(f'Custom   length mean | {sum(custom_file_lengths)/len(custom_file_lengths)}')
+
+
+
+
